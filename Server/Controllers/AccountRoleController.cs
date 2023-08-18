3d2ed98472511e3b3fd4ae0a45b7ee8c2d95e0c4
ahmadeko2017 <44262212+ahmadeko2017@users.noreply.github.com>
2023-08-18 10:26:19 +0700

    Menambahkan appsetting.json

---
+++ Server/Controllers/AccountRoleController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers;
+
+[ApiController]
+[Route("api/account-role")]
+public class AccountRoleController : ControllerBase
+{
+    private readonly AccountRole
+}
